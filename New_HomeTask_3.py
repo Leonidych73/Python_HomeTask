802d50ee8fcc6f89972c672c09f644e2d903da64
leonidych73@gmail.com <Sogtpro73@>
2023-06-19 09:26:54 -0500

    New Commit new rep

---
+++ New_HomeTask_3.py
@@ -0,0 +1,113 @@
+"""HomeWork_3"""
+import itertools
+
+SEP_NUM = 19
+ALIGHT_NUM = 6
+
+# You have list of tuples. Each tuple represents: name, age, some sum, last name, sex
+# Please do such things:
+# 1 - sort list by age and sex fields
+# 2 - somehow you need to get new list as old list without first two elements and last two elements.
+# Print this new list
+# 3 - in new list calculate total numbers of "female"  and "male" and print it as small table.
+# Example:
+# -----------------
+# | sex    | count |
+# -----------------
+# |  female |  11  |
+# |   male  |  23  |
+# -----------------
+#  advice: female and male calculation can be done vs flat list or you can find your own approach ;)
+
+
+people = [
+    ('Alice', 32, 100, 'Johnson', 'female'),
+    ('Bob', 41, 200, 'Smith', 'male'),
+    ('Charlie', 27, 150, 'Jones', 'male'),
+    ('David', 52, 75, 'Williams', 'male'),
+    ('Eve', 18, 300, 'Davis', 'female'),
+    ('Frank', 33, 50, 'Taylor', 'male'),
+    ('Grace', 42, 125, 'Clark', 'female'),
+    ('Henry', 26, 225, 'Lewis', 'male'),
+    ('Ivy', 38, 175, 'Moore', 'female'),
+    ('Jack', 20, 140, 'Harris', 'male'),
+    ('Kate', 37, 110, 'Miller', 'female'),
+    ('Leo', 44, 90, 'Wilson', 'male'),
+    ('Mae', 29, 180, 'Brown', 'female'),
+    ('Nick', 51, 70, 'Davies', 'male'),
+    ('Oliver', 18, 250, 'Collins', 'male'),
+    ('Pete', 36, 160, 'Green', 'male'),
+    ('Quinn', 20, 230, 'Bell', 'female'),
+    ('Remy', 30, 120, 'Foster', 'male'),
+    ('Sara', 28, 140, 'Baker', 'female'),
+    ('Tom', 47, 80, 'Scott', 'male'),
+    ('Ursula', 39, 135, 'Adams', 'female'),
+    ('Vivian', 25, 190, 'Ross', 'female'),
+    ('Wendy', 46, 90, 'Wright', 'female'),
+    ('Xavier', 31, 105, 'Reed', 'male'),
+    ('Yuliana', 22, 200, 'Lopez', 'female'),
+    ('Zack', 48, 60, 'Mitchell', 'male'),
+    ('Adam', 35, 75, 'Davis', 'male'),
+    ('Bella', 27, 125, 'Smith', 'female'),
+    ('Charlie', 44, 115, 'Johnson', 'male'),
+    ('Daisy', 20, 215, 'Miller', 'female'),
+    ('Ethan', 33, 100, 'Taylor', 'male'),
+    ('Fiona', 40, 150, 'Jones', 'female'),
+    ('George', 24, 180, 'Lewis', 'male'),
+    ('Hannah', 22, 200, 'Williams', 'female'),
+    ('Ivan', 29, 160, 'Brown', 'male'),
+    ('Julie', 55, 90, 'Clark', 'female'),
+    ('Kenny', 38, 140, 'Harris', 'male'),
+    ('Luna', 55, 170, 'Smith', 'female'),
+    ('Mike', 55, 55, 'Johnson', 'male')
+]
+
+# 1 - The list sorting by age and sex fields________________________________
+print('1-st task ' + '_' * 50)  # ____________________________________________
+
+from operator import itemgetter
+
+people.sort(key=itemgetter(4, 1))
+for x in people:
+    print(x)  # print list in the column
+
+print('2-nd task ' + '_' * 50)  # _______________________________the_deviding_line
+#  2 - somehow you need to get new list as old list without
+#  first two elements and last two elements. Print this new list
+
+new_people = people
+new_people.pop()
+new_people.pop()
+new_people.pop(0)
+new_people.pop(0)
+for x in new_people:  # print list in the column
+    print(x)
+
+print('3-th task ' + '_' * 50)  # _________________________________________________________
+# 3 - in new list calculate total numbers of "female"  and "male" and print it as small table.
+
+count_male = 0
+count_female = 0
+
+for x in new_people:
+    if x[4] == 'male':
+        count_male += 1
+    else:
+        count_female += 1
+print("-" * SEP_NUM)
+print(f"| {'sex':^{ALIGHT_NUM}} | {'count':^{ALIGHT_NUM}} |")
+print("-" * SEP_NUM)
+print(f"| {'female':^{ALIGHT_NUM}} | {count_female:^{ALIGHT_NUM}} |")
+print(f"| {'male':^{ALIGHT_NUM}} | {count_male:^{ALIGHT_NUM}} |")
+print("-" * SEP_NUM)
+# ____________________________________________________
+print('3-th task with flat list ' + '_' * 50)  # _______________
+
+flat_list = list(itertools.chain.from_iterable(new_people))
+print("-" * SEP_NUM)
+print(f"| {'sex':^{ALIGHT_NUM}} | {'count':^{ALIGHT_NUM}} |")
+print("-" * SEP_NUM)
+print(f"| {'female':^{ALIGHT_NUM}} | {flat_list.count('female'):^{ALIGHT_NUM}} |")
+print(f"| {'male':^{ALIGHT_NUM}} | {flat_list.count('male'):^{ALIGHT_NUM}} |")
+print("-" * SEP_NUM)
+# ____________end-line________________________________
