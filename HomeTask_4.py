60a0fc72b4c35f4ff724b415a13abb1759f16b37
leonidych73@gmail.com <Sogtpro73@>
2023-06-19 23:13:41 -0500

    New 2 commit 6.19.2023 to New_Branch_12

--- HomeTask_4.py
+++ HomeTask_4.py
@@ -1,4 +1,4 @@
-"""HomeTask_4
+"""HomeTask_4.
 
 we have four values w,x,y,z
 write if-elif-else statement that will search minimum value and print smth aka "'y' is minimum value'
@@ -15,7 +15,5 @@ elif y < w and y < x and y < z:
     print("'y' is the minimum value")
 else:
     print("'z' is the minimum value")
-# ___________________________________________________
-
 
 
